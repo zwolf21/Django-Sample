15690518dd59c39e4326d9177f11e47cab8a1a22
zwolf21 <pbr112@naver.com>
2016-07-13 23:14:54 +0900

    ok

---
+++ polls/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, get_object_or_404
+from polls.models import Choice, Question
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+# Create your views here.
+
+
+def index(request):
+	latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+	context = {'latest_question_list':latest_question_list}
+	return render(request,'polls/index.html',context)
+
+def detail(request, question_id):
+	question = get_object_or_404(Question, pk = question_id)
+	context = {'question':question}
+	return render(request, 'polls/detail.html', context)
+
+def vote(request, question_id):
+	p = get_object_or_404(Question, pk = question_id)
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html',{'question':p,'error_message':'You didn`t select a choice'})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results',args=(p.id,)))
+
+def results(request, question_id):
+	question = get_object_or_404(Question, pk = question_id)
+	return render(request, 'polls/results.html',{'question':question})
